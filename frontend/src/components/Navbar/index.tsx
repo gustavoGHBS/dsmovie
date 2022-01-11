4c2006b86da030d870fd42558644a55514e739dc
gustavoGHBS <gustavohbsouza87@gmail.com>
2022-01-11 17:28:22 -0300

    Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -4,18 +4,18 @@ import './styles.css';
 function Navbar() {
     return (
         <header>
-            <nav className="container">
-                <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
-                    <a href="https://github.com/gustavoGHBS">
-                        <div className="dsmovie-contact-container">
-                            <GithubIcon />
-                            <p className="dsmovie-contact-link">/gustavoGHBS</p>
-                        </div>
-                    </a>
+    <nav className="container">
+        <div className="dsmovie-nav-content">
+            <h1>DSMovie</h1>
+            <a href="https://github.com/gustavoGHBS" target="_blank" rel="noreferrer">
+                <div className="dsmovie-contact-container">
+                    <GithubIcon />
+                    <p className="dsmovie-contact-link">/gustavoGHBS</p>
                 </div>
-            </nav>
-        </header>
+            </a>
+        </div>
+    </nav>
+</header>
     );
 }
 
